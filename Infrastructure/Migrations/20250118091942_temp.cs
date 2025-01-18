bf04c71ee1356c2f665e5a0f6f18d5f5eb92ef56
justinwcy <pythonemail20@gmail.com>
2025-01-18 17:48:16 +0800

    Update

---
+++ Infrastructure/Migrations/20250118091942_temp.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class temp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReceivingDate",
+                table: "RefundOrders",
+                newName: "RefundDate");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Vendors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Vendors");
+
+            migrationBuilder.RenameColumn(
+                name: "RefundDate",
+                table: "RefundOrders",
+                newName: "ReceivingDate");
+        }
+    }
+}
