3338c3aa2b51f0ab9ed062852119c2ca11899eb3
justinwcy <pythonemail20@gmail.com>
2025-03-17 08:53:59 +0800

    Update to use utility function

--- Infrastructure/Extensions/Identity/Authorization/DataAccessUtilities.cs
+++ Infrastructure/Extensions/Identity/Authorization/DataAccessUtilities.cs
@@ -1,4 +1,7 @@
-﻿using Application.Constants;
+﻿using System.Globalization;
+using Application.Constants;
+
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace Infrastructure.Extensions.Identity.Authorization
 {
@@ -7,6 +10,34 @@ namespace Infrastructure.Extensions.Identity.Authorization
         public static string GetPolicyString(string accessName, string dataTableName)
         {
             return $"{PolicyConstant.DataAccessPrefix}{accessName}.{dataTableName}";
+        }
+
+        public static async Task<bool> GetExpiryDateClaim(
+            AuthenticationStateProvider authenticationStateProvider,
+            string claimString)
+        {
+            var authState = await authenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            var policyClaim = user.Claims.FirstOrDefault(claim => claim.Type == claimString);
+
+            var isExpired = true;
+            if (policyClaim != null)
+            {
+                var expiryDateString = policyClaim.Value;
+                var expiryDate = DateTime.ParseExact(
+                    expiryDateString,
+                    "yyyy/MMM/dd HH:mm:ss",
+                    CultureInfo.InvariantCulture);
+                var dateNow = DateTime.Now;
+
+                if (expiryDate > dateNow)
+                {
+                    isExpired = false;
+                }
+            }
+
+            return isExpired;
         }
     }
 }
