f325db3e828a590b182a1ea1e08a1e3ccc0fce2c
justinwcy <pythonemail20@gmail.com>
2025-03-30 11:17:48 +0800

    Update to have CSV Utils

---
+++ Application/CsvConverter/GuidListConverter.cs
@@ -0,0 +1,28 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+
+namespace Application.CsvConverter
+{
+    public class GuidListConverter : DefaultTypeConverter
+    {
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            var guidStringArray = text.Split([','], StringSplitOptions.RemoveEmptyEntries);
+
+            var result = guidStringArray.Select(s =>
+            {
+                if (Guid.TryParse(s.Trim(), out var parsedGuid))
+                {
+                    return parsedGuid;
+                }
+                else
+                {
+                    return Guid.Empty; // Or throw an exception, or handle the error as needed
+                }
+            }).ToList();
+
+            return result;
+        }
+    }
+}
---
+++ Infrastructure/Extensions/CsvUtilities.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+using CsvHelper.Configuration;
+
+namespace Infrastructure.Extensions
+{
+    internal static class CsvUtilities
+    {
+        public static CsvConfiguration GetCsvConfiguration()
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HeaderValidated = null, // Disable header validation
+                MissingFieldFound = null // disable missing field validation if needed.
+            };
+
+            return config;
+        }
+    }
+}
